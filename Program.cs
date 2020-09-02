961e91998aa1b0fbc61f7dbfbb832506c005f93a
timothyd1 <52899859+timothyd1@users.noreply.github.com>
2020-09-02 19:54:21 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TechnicalAssignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
